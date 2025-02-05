25146001c3c4ad8d3b2e008996fa460aac07b3ed
alten <alten@DESKTOP-FOMTK9O>
2025-02-05 13:05:36 +0100

    xd

---
+++ SamuraiProject.Library/Interface/IBattle.cs
@@ -0,0 +1,14 @@
+﻿using SamuraiProject.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiProject.Library.Interface
+{
+    public interface IBattle
+    {
+
+    }
+}
---
+++ SamuraiProject.Library/Repositories/BattleRepo.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using SamuraiProject.Library.Interface;
+using SamuraiProject.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiProject.Library.Repositories
+{
+    public class BattleRepo :IBattle
+    {
+        DatabaseContext cxt;
+        public BattleRepo(DatabaseContext context) 
+        {
+
+        }
+    }
+}
